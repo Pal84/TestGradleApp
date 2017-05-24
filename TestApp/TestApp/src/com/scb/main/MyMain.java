259cc150dc7f2cfa772e4b1b88f6f0e1c907c4ce
lnc <lnc@DIN76000189.corp.capgemini.com>
2017-05-24 17:52:16 +0530

    Head commit

--- TestApp/TestApp/src/com/scb/main/MyMain.java
+++ TestApp/TestApp/src/com/scb/main/MyMain.java
@@ -1,8 +1,9 @@
 package com.scb.main;
 
-public class Main {
+public class MyMain {
 
 	public static void main(String[] args) {
-		System.out.println("Helo World !");
+		System.out.println("Hello World !");
 	}
+	
 }
