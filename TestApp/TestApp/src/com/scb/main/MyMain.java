0d00d4508e71ce1855e35c52fff26280ab44f555
lnc <lnc@DIN76000189.corp.capgemini.com>
2017-05-24 18:02:26 +0530

    Ignored files

--- TestApp/TestApp/src/com/scb/main/MyMain.java
+++ TestApp/TestApp/src/com/scb/main/MyMain.java
@@ -3,7 +3,7 @@ package com.scb.main;
 public class MyMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println("Hello World ! Welcome to the world of GIT !");
 	}
 	
 }
